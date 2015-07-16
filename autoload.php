7d009d7844a5246c45745c483ce25c3926ff5338
Oli Griffiths <oli@tumblr.com>
2015-07-16 18:30:59 -0400

    Restructure

--- autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Include the class loader
-require_once dirname(__FILE__).'/lib/class/loader.php';
-
-//Make class loader config available
-global $class_loader_config;
-
-Nooku\Library\ClassLoader::getInstance($class_loader_config);
